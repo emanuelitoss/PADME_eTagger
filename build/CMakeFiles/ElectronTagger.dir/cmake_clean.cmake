4058c278becdea8757e87a59c295d1e1d372ef47
Emanuele <rosi.1812180@studenti.uniroma1.it>
2022-10-03 23:47:12 +0200

    Aggiusto il .gitignore

--- build/CMakeFiles/ElectronTagger.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/ElectronTagger"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ElectronTagger.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
