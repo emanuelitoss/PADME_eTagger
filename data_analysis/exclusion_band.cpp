ea07ab76edcc9b75038eeea2d560a749fde0d496
Emanuele <rosi.1812180@studenti.uniroma1.it>
2022-11-15 15:41:29 +0100

    Tolgo la somma degli scatterplot, che tanto non è indicativo di nulla. Inoltre modifico il fit, che comunuque ancora non è abbastanza buono...

--- data_analysis/exclusion_band.cpp
+++ data_analysis/exclusion_band.cpp
@@ -22,9 +22,9 @@ int main(int argc, char** argv){
 
     // charges vs arrival time >> for scatterplot
     // channels 0,1 for left and right sides of SiPMs
-    // channel 2,3 for the difference and sum
-    vector <vector <double_t> > charge_points = {{}, {}, {}, {}};
-    vector <vector <double_t> > time_points = {{}, {}, {}, {}};
+    // channel 2 for the difference
+    vector <vector <double_t> > charge_points = {{}, {}, {}};
+    vector <vector <double_t> > time_points = {{}, {}, {}};
 
     /****************** READING & STORE DATA ******************/
     TTreeReader readerQ = TTreeReader();
@@ -78,11 +78,9 @@ int main(int argc, char** argv){
         charge_points[0].push_back(charge_dx);
         charge_points[1].push_back(charge_sx);
         charge_points[2].push_back(charge_dx - charge_sx);
-        charge_points[3].push_back(charge_dx + charge_sx);
         time_points[0].push_back(time_dx);
         time_points[1].push_back(time_sx);
         time_points[2].push_back(time_dx - time_sx);
-        time_points[3].push_back(time_dx + time_sx);
     }
 
     // check the right behavior of the program & data
@@ -136,7 +134,7 @@ int main(int argc, char** argv){
 
         graph->Draw("ap");
         
-        if(ch<2) fit = new TF1("exponential function", "[0] + [1]*exp(-[2]*x) + [3]/x", graph->GetXaxis()->GetXmin(), graph->GetXaxis()->GetXmax());
+        if(ch<2) fit = new TF1("exponential function", "[1] + [2]*exp(-[3]*x)", graph->GetXaxis()->GetXmin(), graph->GetXaxis()->GetXmax());
         else fit = new TF1("Poly3", "pol3", graph->GetXaxis()->GetXmin(), graph->GetXaxis()->GetXmax());
         graph->Fit(fit, "0", "0");
         fit->SetLineColor(kBlack);
@@ -144,16 +142,17 @@ int main(int argc, char** argv){
         fit->Draw("SAME");
 
         leg = new TLegend(0.3, 0.78, 0.89, 0.89);
-        leg->SetHeader("Fit results:","");
         if(ch<2)
         {
-            leg->AddEntry("-", Form("quote: %.4g +/- %.4g",fit->GetParameter(0), fit->GetParError(0)),"L");
-            leg->AddEntry("-", Form("coefficient: %.4g +/- %.4g",fit->GetParameter(1), fit->GetParError(1)), "L");
-            leg->AddEntry("-", Form("decayconstant: %.4g +/- %.4g",fit->GetParameter(2), fit->GetParError(2)), "L");
-            leg->AddEntry("-", Form("1/x term: %.4g +/- %.4g",fit->GetParameter(3), fit->GetParError(3)), "L");
+            leg->SetHeader("f(x) = A + B*exp(-C*x):","");
+            //leg->AddEntry("-", Form("quote: %.4g +/- %.4g",fit->GetParameter(0), fit->GetParError(0)),"L");
+            leg->AddEntry("-", Form("A = %.4g +/- %.4g",fit->GetParameter(1), fit->GetParError(1)), "L");
+            leg->AddEntry("-", Form("B = %.4g +/- %.4g",fit->GetParameter(2), fit->GetParError(2)), "L");
+            leg->AddEntry("-", Form("C = %.4g +/- %.4g",fit->GetParameter(3), fit->GetParError(3)), "L");
         }
         else
         {
+            leg->SetHeader("Polynomial of 3rd degree:","");
             leg->AddEntry("-", Form("par0: %.4g +/- %.4g",fit->GetParameter(0), fit->GetParError(0)),"L");
             leg->AddEntry("-", Form("par1: %.4g +/- %.4g",fit->GetParameter(1), fit->GetParError(1)), "L");
             leg->AddEntry("-", Form("par2: %.4g +/- %.4g",fit->GetParameter(2), fit->GetParError(2)), "L");
